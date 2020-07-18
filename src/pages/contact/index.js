8f7d0144105e86971ae831c8ad87114b4b7adcf1
LanceJernigan <lance@lancejernigan.com>
2020-07-18 18:28:02 -0400

    Add contact page

---
+++ src/pages/contact/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Page from '../../components/shared/page';
+
+export default () => (
+    <Page>
+        <p>Contact Hero</p>
+        <p>Contact Emails</p>
+    </Page>
+);
