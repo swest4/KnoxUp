d0770cfb4d26be65ddb6a829e6d4c286bf9f5982
LanceJernigan <lance@lancejernigan.com>
2020-07-18 18:51:46 -0400

    Setup Routes

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-  } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './pages/home';
+import Contact from './pages/contact';
+import Projects from './pages/projects';
 
 export default () => (
     <Router>
         <Switch>
-            <Route path="/contact"><h1>Contact</h1></Route>
-            <Route path="/projects"><h1>Projects</h1></Route>
-            <Route path="/"><h1>Home</h1></Route>
+            <Route path="/contact/"><Contact /></Route>
+            <Route path="/projects/"><Projects /></Route>
+            <Route path="/"><Home /></Route>
         </Switch>
     </Router>
-)
+);
