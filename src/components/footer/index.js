4d50f509ed4fd038351575584ff275aea0b77161
LanceJernigan <lance@lancejernigan.com>
2020-07-18 19:09:43 -0400

    Add base footer structure

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Styles from './styled';
 
-export default () => <Styles><p>Footer</p></Styles>;
+export default () => <Styles>
+    <p>Logo</p>
+    <ul>
+        <li>
+            <Link to="/projects/">Projects</Link>
+
+            <ul>
+                <li><Link to="/projects/map/">Map</Link></li>
+                <li><Link to="/projects/timeline/">Timeline</Link></li>
+                <li><Link to="/projects/list/">List</Link></li>
+            </ul>
+        </li>
+        <li><Link to="/contact/">Contact</Link></li>
+    </ul>
+</Styles>;
