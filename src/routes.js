3c14ce2bdfe8620a3b621c4328aaf9a53fe79468
Lance Jernigan <Lance@LanceJernigan.com>
2020-07-18 19:13:18 -0400

    Feature/footer base (#40)
    * Add React Router dependency
    
    * dependencies
    
    * Move Router to App
    
    * Add base footer structure
    
    Co-authored-by: Joyanna Hirst <jhirst@pyxl.com>

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from './pages/home';
 import Contact from './pages/contact';
 import Projects from './pages/projects';
 
 export default () => (
-    <Router>
-        <Switch>
-            <Route path="/contact/" component={Contact} />
-            <Route path="/projects/" component={Projects} />
-            <Route path="/" component={Home} />
-        </Switch>
-    </Router>
+    <Switch>
+        <Route path="/contact/" component={Contact} />
+        <Route path="/projects/" component={Projects} />
+        <Route path="/" component={Home} />
+    </Switch>
 );
