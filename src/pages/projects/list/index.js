40e75eb6723b009987cfb6a45d8170fac41f5299
LanceJernigan <lance@lancejernigan.com>
2020-07-18 18:50:32 -0400

    Setup Projects List Page

---
+++ src/pages/projects/list/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Page from '../../../components/shared/page';
+
+export default () => (
+    <Page>
+        <p>List Filters</p>
+        <p>List</p>
+    </Page>
+);
