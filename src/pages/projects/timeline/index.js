84fc780c3174ba2c165f22017adebd9a3f7af539
Lance Jernigan <Lance@LanceJernigan.com>
2020-07-18 18:54:15 -0400

    Feature/setup pages (#39)
    * Add React Router dependency
    
    * dependencies
    
    * Add page component
    
    * Add contact page
    
    * Add home page
    
    * Setup Projects List Page
    
    * Setup Projects Map Page
    
    * Setup Projects Single Page
    
    * Setup Project Timeline Page
    
    * Setup Projects Page
    
    * Setup Routes
    
    * Restructure Routes
    
    Co-authored-by: Joyanna Hirst <jhirst@pyxl.com>

---
+++ src/pages/projects/timeline/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Page from '../../../components/shared/page';
+
+export default () => (
+    <Page>
+        <p>Timeline Filters</p>
+        <p>Timeline</p>
+    </Page>
+);
