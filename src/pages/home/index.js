e47c06af8f590fb592d384cd980e8d13a5bdecf8
LanceJernigan <lance@lancejernigan.com>
2020-07-18 18:28:07 -0400

    Add home page

---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from '../../components/shared/page';
+
+export default () => (
+    <Page>
+        <p>Hero</p>
+        <p>Map CTA</p>
+        <p>Timeline CTA</p>
+        <p>List CTA</p>
+        <p>About</p>
+    </Page>
+);
