4b3d91b4229cd6714401b40d5a76d66f0c0dd574
LanceJernigan <lance@lancejernigan.com>
2020-07-18 22:07:18 -0400

    Add sub nav variables

--- src/theme.js
+++ src/theme.js
@@ -1,50 +1,56 @@
-export default {
-  color: {
-    blue: "#579CF0",
-    blurple: "#4F28ED",
-    purple: "#8D6AF6",
-    yellow: "#EDBD59",
-    navy: "#060321",
-    white: "#FFFFFF",
-  },
-  gradients: {
-    n2p: "linear-gradient(#000745, #4F28ED, #8D6AF6)",
-    b2p: "linear-gradient(#4F28ED, #8D6AF6)",
-    n2b: "linear-gradient(#000745, #4F28ED, #579CF0)",
-  },
-  font: {
-    primary: "fira-sans",
-  },
-  type: {
-    h1: "3.052rem",
-    h2: "2.441rem",
-    h3: "1.953rem",
-    h4: "1.563rem",
-    h5: "1.25rem",
-    h6: "1rem",
-    p: "1rem",
-    label: "0.8rem",
-  },
-  lineHeight: {
-    tight: "1.2em",
-    normal: "1.45em",
-    loose: "1.7em",
-  },
-  fontWeight: {
-    regular: "400",
-    medium: "500",
-    semibold: "600",
-    bold: "700",
-    extraBold: "800",
-  },
-  letterSpacing: {
-    tight: ".01em",
-    normal: ".018em",
-    loose: ".026em",
-  },
-  breakpoints: {
-    mobile: "320px",
-    tablet: "768px",
-    desktop: "1024px",
-  },
-};
+export default ({
+    color: {
+        blue: '#579CF0',
+        blurple: '#4F28ED',
+        purple: '#8D6AF6',
+        yellow: '#EDBD59',
+        navy: '#060321',
+        white: '#FFFFFF',
+    },
+    gradients: {
+      n2p: "linear-gradient(#000745, #4F28ED, #8D6AF6)",
+      b2p: "linear-gradient(#4F28ED, #8D6AF6)",
+      n2b: "linear-gradient(#000745, #4F28ED, #579CF0)",
+    },
+    font: {
+        primary: 'fira-sans',
+    },
+    type: {
+        h1: '3.052rem',
+        h2: '2.441rem',
+        h3: '1.953rem',
+        h4: '1.563rem',
+        h5: '1.25rem',
+        h6: '1rem',
+        p: '1rem',
+        label: '0.8rem',
+    },
+    lineHeight: {
+        tight: '1.2em',
+        normal: '1.45em',
+        loose: '1.7em',
+    },
+    fontWeight: {
+        regular: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extraBold: '800',
+    },
+    letterSpacing: {
+        tight: '.01em',
+        normal: '.018em',
+        loose: '.026em',
+    },
+    breakpoints: {
+        mobile: '320px',
+        tablet: '768px',
+        desktop: '1024px',
+    },
+    maxWidth: {
+        container: '1200px',
+    },
+    borderRadius: {
+        subNavLink: '1em',
+    }
+});
