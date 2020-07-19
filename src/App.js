1a32f2f036c8672c49690109d9e23f803679c664
LanceJernigan <lance@lancejernigan.com>
2020-07-19 01:34:32 -0400

    Remove rdsData from app

--- src/App.js
+++ src/App.js
@@ -7,22 +7,29 @@ import GlobalStyles from './Global.styled.js';
 import Header from './components/header';
 import Footer from './components/footer';
 import Routes from './routes';
-import useRdsData from './hooks/useRdsData';
 import theme from './theme';
 
 const App = () => {
-  const rdsData = useRdsData();
-
-  if(rdsData) {
-    console.log(rdsData);
-  }
-
   return (
     <Router>
       <Helmet>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
