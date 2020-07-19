0c1c3a0b0c97a2fd66557ca1ce920448fecf1b6c
LanceJernigan <lance@lancejernigan.com>
2020-07-19 05:53:02 -0400

    Structure homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,60 @@
-import React from "react";
-import Page from "../../components/shared/page";
-import Hero from "../../components/hero";
+import React from 'react';
+import Page from '../../components/shared/page';
+import Hero from '../../components/hero';
+import Content from '../../components/content';
+import BlurbsThree from '../../components/blurbsThree';
+import ButtonLink from '../../components/shared/buttonLink';
+import ExploreIcon from '../../svgs/explore';
+import TimelineIcon from '../../svgs/timeline';
+import ListIcon from '../../svgs/list';
 
 export default () => (
   <Page>
     <Hero />
-    <p>Map CTA</p>
-    <p>Timeline CTA</p>
-    <p>List CTA</p>
-    <p>About</p>
+
+    <Content>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </Content>
+
+    <BlurbsThree
+      columnOne={
+        <>
+          <ExploreIcon />
+          <p>
+            Use our interactive map to view all projects the City of Knoxville
+            and Knox County have invested in.
+          </p>
+          <ButtonLink to="/projects/map/">Get Started</ButtonLink>
+        </>
+      }
+      columnTwo={
+        <>
+          <TimelineIcon />
+          <p>
+            Use our interactive timeline to view all projects the City of
+            Knoxville and Knox County have invested in.
+          </p>
+          <ButtonLink to="/projects/map/">Get Started</ButtonLink>
+        </>
+      }
+      columnThree={
+        <>
+          <ListIcon />
+          <p>
+            Use our interactive list to view all projects the City of Knoxville
+            and Knox County have invested in.
+          </p>
+          <ButtonLink to="/projects/map/">Get Started</ButtonLink>
+        </>
+      }
+    />
   </Page>
 );
