f4e048c4f256f69bec3f56e726cb71846df7e909
LanceJernigan <lance@lancejernigan.com>
2020-07-19 11:15:33 -0400

    Fade in animations

--- src/components/heroProject/index.js
+++ src/components/heroProject/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { useInView } from "react-intersection-observer";
-import Styles from "./styled";
+import React from 'react';
+import { useInView } from 'react-intersection-observer';
+import Styles from './styled';
 
-export default ({ title = "", subTitle = "", children }) => {
+export default ({ title = '', subTitle = '', children }) => {
   const [ref, inView] = useInView({ threshold: 0.25, triggerOnce: true });
 
   return (
-    <Styles ref={ref} className={inView ? "active" : ""}>
+    <Styles ref={ref} className={inView ? 'active' : ''}>
       <div className="inner">
         <h1>{title}</h1>
         <h2>{subTitle}</h2>
