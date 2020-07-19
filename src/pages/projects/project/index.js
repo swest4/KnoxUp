1a6d83016f54a0a48bd080c2ebc654d1f0d40eb5
LanceJernigan <lance@lancejernigan.com>
2020-07-19 05:25:49 -0400

    Add project overview

--- src/pages/projects/project/index.js
+++ src/pages/projects/project/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Page from '../../../components/shared/page';
 import ProjectHero from '../../../components/heroProject';
+import ProjectOverview from '../../../components/projectOverview';
 
-export default ({ project_name, project_address, end_date, ...rest }) =>
-  console.log(rest) || (
-    <Page>
-      <ProjectHero title={project_name} subTitle={project_address} />
-      <p style={{ minHeight: '100vh' }}>Project Overview</p>
-    </Page>
-  );
+export default ({ project_name, project_address, ...rest }) => (
+  <Page>
+    <ProjectHero title={project_name} subTitle={project_address} />
+    <ProjectOverview {...rest} />
+  </Page>
+);
