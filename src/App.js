c071cff37bbb5be5b9c0b9f11ff73fa3e264aba8
swest4 <swest4@gmail.com>
2020-07-18 21:04:16 -0400

    Feature/data hook (#41)
    * Add logo and favicon. Add useRdsData hook.
    
    * Add logo and favicon. Add useRdsData hook.
    
    * Update package lock
    
    * clean up unused items
    
    Co-authored-by: Shane West <shane.west@regalcinemas.com>
    Co-authored-by: LanceJernigan <lance@lancejernigan.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Helmet from 'react-helmet';
 import { ThemeProvider } from 'emotion-theming';
 import AppStyles from './App.styled.js';
 import Header from './components/header';
 import Footer from './components/footer';
 import Routes from './routes';
+import useRdsData from './hooks/useRdsData';
 import theme from './theme';
 
-function App() {
+const App = () => {
+  const rdsData = useRdsData();
+
+  if(rdsData) {
+    console.log(rdsData);
+  }
+
   return (
     <Router>
+      <Helmet>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Helmet>
       <ThemeProvider theme={theme}>
         <AppStyles className="App">
           <Header />
@@ -19,6 +36,6 @@ function App() {
       </ThemeProvider>
     </Router>
   );
-}
+};
 
 export default App;
