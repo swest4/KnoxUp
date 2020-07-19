0d3281848ff4c3141fe51933ab73d2e326d8eb15
LanceJernigan <lance@lancejernigan.com>
2020-07-19 09:04:56 -0400

    Animate list intro

--- src/components/projectsList/index.js
+++ src/components/projectsList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { useInView } from 'react-intersection-observer';
 import Styles from './styled';
@@ -9,34 +10,40 @@ export default ({ projects = [] }) => {
 
   return (
     <Styles ref={ref} className={inView ? 'active' : ''}>
-      <div className="inner">
-        <div className="wrapper">
-          <table>
-            <thead>
-              <tr>
-                <td>Project Name</td>
-                <td>Project Address</td>
-                <td>Completion Year</td>
-                <td></td>
-              </tr>
-            </thead>
-            <tbody>
-              {projects.map(({ project_name, project_address, end_date }) => (
+      {projects.length && (
+        <motion.div
+          className="inner"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transform={{ duration: 0.4 }}>
+          <div className="wrapper">
+            <table>
+              <thead>
                 <tr>
-                  <td>{project_name}</td>
-                  <td>{project_address}</td>
-                  <td>{end_date}</td>
-                  <td>
-                    <Link to={`/projects/${toKebabCase(project_name)}/`}>
-                      Read more
-                    </Link>
-                  </td>
+                  <td>Project Name</td>
+                  <td>Project Address</td>
+                  <td>Completion Year</td>
+                  <td></td>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
+              </thead>
+              <tbody>
+                {projects.map(({ project_name, project_address, end_date }) => (
+                  <tr>
+                    <td>{project_name}</td>
+                    <td>{project_address}</td>
+                    <td>{end_date}</td>
+                    <td>
+                      <Link to={`/projects/${toKebabCase(project_name)}/`}>
+                        Read more
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </motion.div>
+      )}
     </Styles>
   );
 };
