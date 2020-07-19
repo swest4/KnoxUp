8ffabffe6986f7c6fd5558addb5c29530f613d67
Lance Jernigan <Lance@LanceJernigan.com>
2020-07-18 20:15:48 -0400

    Feature/theme base (#42)
    * Add emotion theming dependency
    
    * Setup theme provider
    
    * Setup theme
    
    * Add global font styles

---
+++ src/theme.js
@@ -0,0 +1,39 @@
+export default ({
+    color: {
+        blue: '#579CF0',
+        blurple: '#4F28ED',
+        purple: '#8D6AF6',
+        yellow: '#EDBD59',
+        navy: '#060321'
+    },
+    font: {
+        primary: 'fira-sans',
+    },
+    type: {
+        h1: '3.052rem',
+        h2: '2.441rem',
+        h3: '1.953rem',
+        h4: '1.563rem',
+        h5: '1.25rem',
+        h6: '1rem',
+        p: '1rem',
+        label: '0.8rem',
+    },
+    lineHeight: {
+        tight: '1.2em',
+        normal: '1.45em',
+        loose: '1.7em',
+    },
+    fontWeight: {
+        regular: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extraBold: '800',
+    },
+    letterSpacing: {
+        tight: '.01em',
+        normal: '.018em',
+        loose: '.026em',
+    }
+});
