164d1df3701fe2d987fb84ff3d2bd1f6903749ac
LanceJernigan <lance@lancejernigan.com>
2020-07-19 04:12:52 -0400

    Setup project hero block

---
+++ src/components/heroProject/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useInView } from 'react-intersection-observer';
+import Styles from './styled';
+
+export default ({ title = '', subTitle = '' }) => {
+  const [ref, inView] = useInView({ threshold: 0.25, triggerOnce: true });
+
+  return (
+    <Styles ref={ref} className={inView ? 'active' : ''}>
+      <div className="inner">
+        <h1>{title}</h1>
+        <h2>{subTitle}</h2>
+      </div>
+    </Styles>
+  );
+};
