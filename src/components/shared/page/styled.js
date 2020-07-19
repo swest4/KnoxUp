59ffa536eeb17017551d0db4a25ae6922e9d1d45
LanceJernigan <lance@lancejernigan.com>
2020-07-19 03:27:29 -0400

    Style page

--- src/components/shared/page/styled.js
+++ src/components/shared/page/styled.js
@@ -1,3 +1,9 @@
 import styled from '@emotion/styled';
 
-export default styled.article``;
+export default styled.article`
+  display: flex;
+  flex-direction: column;
+  max-height: 100%;
+  overflow: scroll;
+  position: relative;
+`;
