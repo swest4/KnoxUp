03813e6b077717c748ead6be3cebe9df1586c317
Lance Jernigan <Lance@LanceJernigan.com>
2020-07-19 01:26:34 -0400

    Feature/header (#52)
    * Fix overflow
    
    * Update routes
    
    * Add variables
    
    * Setup header styles
    
    * Add icons
    
    * Fix overflow
    
    * Update routes
    
    * Add variables
    
    * Setup header styles
    
    * Add icons
    
    * Update subnav link color

--- src/components/subNav/styled.js
+++ src/components/subNav/styled.js
@@ -1,49 +1,54 @@
 import styled from '@emotion/styled';
 
 export default styled.nav`
-    background: ${props => props.theme.color.blurple};
-    padding: .5em 1em;
-    position: sticky;
-    top: 0;
+  background: ${(props) => props.theme.color.blurple};
+  padding: 0.5em 1em;
+  position: sticky;
+  top: 0;
 
-    ul {
-        max-width: ${props => props.theme.maxWidth.container};
-        margin: 0 auto;
-        padding: 0;
-        list-style: none;
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
+  ul {
+    max-width: ${(props) => props.theme.maxWidth.container};
+    margin: 0 auto;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+
+  li {
+    margin: 0 0.5rem;
+
+    &:first-child {
+      margin-left: 0;
     }
 
-    li {
-        margin: 0 .5rem;
-
-        &:first-child {
-            margin-left: 0;
-        }
-
-        &:last-child {
-            margin-right: 0;
-        }
+    &:last-child {
+      margin-right: 0;
     }
+  }
 
-    a {
-        font-size: ${props => props.theme.type.label};
-        
-        display: inline-block;
-        padding: .5em 1.5em;
-        text-decoration: none;
-        color: ${props => props.theme.color.white};
-    }
+  a {
+    font-size: ${(props) => props.theme.type.label};
+    display: inline-block;
+    padding: 0.5em 1.5em;
+    text-decoration: none;
+    color: ${(props) => props.theme.color.white};
 
-    .indicator {
-        border-radius: ${props => props.theme.borderRadius.subNavLink};
-        background: rgba(0, 0, 0, .2);
-        height: calc(100% - 1rem);
-        position: absolute;
-        top: .5rem;
-        transition: all .4s ease-in-out;
-        z-index: -1;
+    &:active,
+    &:focus,
+    &:hover {
+      color: ${(props) => props.theme.color.white};
     }
+  }
+
+  .indicator {
+    border-radius: ${(props) => props.theme.borderRadius.subNavLink};
+    background: rgba(0, 0, 0, 0.2);
+    height: calc(100% - 1rem);
+    position: absolute;
+    top: 0.5rem;
+    transition: all 0.4s ease-in-out;
+    z-index: -1;
+  }
 `;
