3fee11ae4bf38a1e19b79e8aa3c5dce2884c7a18
LanceJernigan <lance@lancejernigan.com>
2020-07-19 01:22:26 -0400

    Setup header styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,57 @@
-import React from "react";
-import Styles from "./styled";
-import Button from "../shared/buttonChip";
-import A from "../shared/buttonLink";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Styles from './styled';
+import Logo from '../../svgs/Logo';
+import BusinessIcon from '../../svgs/business';
+import ExploreIcon from '../../svgs/explore';
+import TimelineIcon from '../../svgs/timeline';
+import ListIcon from '../../svgs/list';
+import EmailIcon from '../../svgs/email';
 
 export default () => (
   <Styles>
-    <p>Header</p>
-    <Button>Stuff</Button>
-    <A>Stuff</A>
+    <ul>
+      <li>
+        <NavLink to="/" exact>
+          <Logo className="logo" />
+          <p>Home</p>
+        </NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/projects/">
+          <BusinessIcon />
+          <p>Projects</p>
+        </NavLink>
+
+        <ul>
+          <li>
+            <NavLink to="/projects/map/">
+              <ExploreIcon />
+              <p>Map</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects/timeline/">
+              <TimelineIcon />
+              <p>Timeline</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects/list/">
+              <ListIcon />
+              <p>List</p>
+            </NavLink>
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        <NavLink to="/contact/">
+          <EmailIcon />
+          <p>Contact</p>
+        </NavLink>
+      </li>
+    </ul>
   </Styles>
 );
