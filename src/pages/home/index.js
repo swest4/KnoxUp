0d2ee2eca2bd26a1b1fa40765ef5c228c85b2357
Lance Jernigan <Lance@LanceJernigan.com>
2020-07-19 19:37:15 -0400

    Bugfix/content hiding (#71)
    * update lock
    
    * Cleanup content styling

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,31 +1,31 @@
-import React from "react";
-import Page from "../../components/shared/page";
-import Hero from "../../components/hero";
-import Content from "../../components/content";
-import BlurbsThree from "../../components/blurbsThree";
-import ButtonLink from "../../components/shared/buttonLink";
-import ExploreIcon from "../../svgs/explore";
-import TimelineIcon from "../../svgs/timeline";
-import ListIcon from "../../svgs/list";
+import React from 'react';
+import Page from '../../components/shared/page';
+import Hero from '../../components/hero';
+import Content from '../../components/content';
+import BlurbsThree from '../../components/blurbsThree';
+import ButtonLink from '../../components/shared/buttonLink';
+import ExploreIcon from '../../svgs/explore';
+import TimelineIcon from '../../svgs/timeline';
+import ListIcon from '../../svgs/list';
 
 export default () => (
   <Page>
     <Hero />
 
-    <Content>
+    <Content align="left">
       <h2>Enriching Our Communities</h2>
-      <blockquote>
+      <p>
         In Knoxville and Knox County, the Payment-In-Lieu-Of-Taxes (PILOT)
         program is targeted at the development of property for either jobs
         creating economic development projects or economic-catalyst projects. In
         both cases, the goal is to appropriately incentivize business owners and
         developers to create an economic activity where there was none before.
-      </blockquote>
-      <h4>
+      </p>
+      <p className="">
         KnoxUp is an archive of projects related to the PILOT program. There are
         three different ways to view the data on each project. You can choose
-        between list, timeline and map view.{" "}
-      </h4>
+        between list, timeline and map view.{' '}
+      </p>
     </Content>
 
     <BlurbsThree
@@ -45,7 +45,7 @@ export default () => (
           <p>
             The Timeline view shows a list of projects in sequential order. You
             can easily scroll through the years and see how Knoxville has
-            developed{" "}
+            developed{' '}
           </p>
           <ButtonLink to="/projects/timeline/">Get Started</ButtonLink>
         </>
