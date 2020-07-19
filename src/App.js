7b1cbdf8d677d2601f014a57aa52fb2e6e930067
LanceJernigan <lance@lancejernigan.com>
2020-07-18 20:51:37 -0400

    Add global styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'emotion-theming';
 import AppStyles from './App.styled.js';
+import GlobalStyles from './Global.styled.js';
 import Header from './components/header';
 import Footer from './components/footer';
 import Routes from './routes';
@@ -11,10 +12,13 @@ function App() {
   return (
     <Router>
       <ThemeProvider theme={theme}>
+        <GlobalStyles />
         <AppStyles className="App">
           <Header />
-          <Routes />
-          <Footer />
+          <section className="content">
+            <Routes />
+            <Footer />
+          </section>
         </AppStyles>
       </ThemeProvider>
     </Router>
