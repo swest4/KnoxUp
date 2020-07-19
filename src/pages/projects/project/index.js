9575868ec71e0102a1f3db7168b59d9482736639
LanceJernigan <lance@lancejernigan.com>
2020-07-19 04:13:37 -0400

    Add project hero

--- src/pages/projects/project/index.js
+++ src/pages/projects/project/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Page from '../../../components/shared/page';
+import ProjectHero from '../../../components/heroProject';
 
-export default ({ match: { params: { projectId = false } = {} } = {} }) => (
+export default ({ project_name, project_address, end_date, ...rest }) =>
+  console.log(rest) || (
     <Page>
-        <p>Project Hero - {projectId}</p>
-        <p>Project Overview</p>
+      <ProjectHero title={project_name} subTitle={project_address} />
+      <p style={{ minHeight: '100vh' }}>Project Overview</p>
     </Page>
-);
+  );
