0cf439286be2f31e01dd3ad4dd8a4fc6a356dca5
LanceJernigan <lance@lancejernigan.com>
2020-07-18 20:11:13 -0400

    Setup theme provider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'emotion-theming';
 import AppStyles from './App.styled.js';
 import Header from './components/header';
 import Footer from './components/footer';
 import Routes from './routes';
+import theme from './theme';
 
 function App() {
   return (
     <Router>
-      <AppStyles className="App">
-        <Header />
-        <Routes />
-        <Footer />
-      </AppStyles>
+      <ThemeProvider theme={theme}>
+        <AppStyles className="App">
+          <Header />
+          <Routes />
+          <Footer />
+        </AppStyles>
+      </ThemeProvider>
     </Router>
   );
 }
