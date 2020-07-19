f5581d65247097251299695c7123c478067db759
helloidevthings <helloidevthings@gmail.com>
2020-07-19 00:05:13 -0400

    buttons and theme styling (#47)
    * buttons and theme styling
    
    * buttons and theme styling

--- src/Global.styled.js
+++ src/Global.styled.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import { Global, css } from '@emotion/core';
+import React from "react";
+import { Global, css } from "@emotion/core";
 
 export default () => (
-    <Global styles={
-        css`
-            body, html {
-                margin: 0;
-                padding: 0;
-            }
-        `
-    } />
-)
+  <Global
+    styles={css`
+      body,
+      html {
+        margin: 0;
+        padding: 0;
+      }
+      body {
+        color: white;
+        background: linear-gradient(45deg, #060321, #2d224f);
+      }
+    `}
+  />
+);
