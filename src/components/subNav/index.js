6cc2955dc00988fbd2a7b28a3292ba93fd1bb12a
LanceJernigan <lance@lancejernigan.com>
2020-07-18 22:45:41 -0400

    Add sliding indicator

--- src/components/subNav/index.js
+++ src/components/subNav/index.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { createRef, useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import Styles from './styled';
 
 export default ({
     links = []
-}) => (
-    <Styles>
-        <ul>
-            {
-                links.map(
-                    ({ to, title }) => (
-                        <li key={to}>
-                            <NavLink to={to}>{title}</NavLink>
-                        </li>
+}) => {
+    const [indicatorStyles, setIndicatorStyles] = useState({ width: 0, left: 0});
+    const listRef = createRef(null);
+
+    useEffect(() => {
+        const activeChild = listRef.current.querySelector('.active');
+        const { offsetWidth: width, offsetLeft: left } = activeChild;
+
+        if (indicatorStyles.width !== width || indicatorStyles.left !== left) {
+            setIndicatorStyles({
+                width: activeChild.offsetWidth,
+                left: activeChild.offsetLeft
+            });
+        }
+    });
+
+    return (
+        <Styles>
+            <ul ref={listRef}>
+                {
+                    links.map(
+                        ({ to, title }) => (
+                            <li key={to}>
+                                <NavLink to={to}>{title}</NavLink>
+                            </li>
+                        )
                     )
-                )
-            }
-        </ul>
-    </Styles>
-);
+                }
+            </ul>
+            <div className="indicator" style={{ width: indicatorStyles.width, left: indicatorStyles.left }} />
+        </Styles>
+    );
+};
