c6bb4410587f525da7e341b011a3bc6a80b9db33
Shane West <shane.west@regalcinemas.com>
2020-07-19 01:01:29 -0400

    add start of maps

---
+++ src/components/map/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Styles from "./styled";
+import Button from "../shared/buttonChip";
+import A from "../shared/buttonLink";
+
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+export default () => {
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return (
+    <LoadScript
+      googleMapsApiKey="AIzaSyCsAfa4WqASPDbyECxtESeuJp66WC2kYzM"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  )
+}
