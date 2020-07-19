8196042496736cd1198aa2ca47a19a5e89d41363
Lance Jernigan <Lance@LanceJernigan.com>
2020-07-19 08:11:06 -0400

    done (#61)

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,9 +1,57 @@
 import React from 'react';
 import Page from '../../components/shared/page';
+import Hero from '../../components/heroProject';
+import ContactCards from '../../components/contactCards';
+
+const contacts = [
+  {
+    name: 'Joyanna Hirst',
+    social: [
+      {
+        type: 'email',
+        value: 'joyannahirst@gmail.com',
+      },
+      {
+        type: 'twitter',
+        value: 'https://twitter.com/joyannahirst',
+      },
+      {
+        type: 'linkedin',
+        value: 'https://www.linkedin.com/in/joyanna-hirst-88ab5932/',
+      },
+    ],
+  },
+  {
+    name: 'Lance Jernigan',
+    social: [
+      {
+        type: 'email',
+        value: 'lance@lancejernigan.com',
+      },
+      {
+        type: 'linkedin',
+        value: 'https://www.linkedin.com/in/lancejernigan',
+      },
+      {
+        type: 'twitter',
+        value: 'https://twitter.com/lance_jernigan',
+      },
+    ],
+  },
+  {
+    name: 'Shane West',
+    social: [
+      {
+        type: 'email',
+        value: 'swest4@gmail.com',
+      },
+    ],
+  },
+];
 
 export default () => (
-    <Page>
-        <p>Contact Hero</p>
-        <p>Contact Emails</p>
-    </Page>
+  <Page>
+    <Hero title="Contact" />
+    <ContactCards contacts={contacts} />
+  </Page>
 );
