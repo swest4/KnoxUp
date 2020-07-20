a94b8171d0c29e1e697694510b915e1f73978fac
LanceJernigan <lance@lancejernigan.com>
2020-07-19 20:30:05 -0400

    Add env api key

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { GoogleMap, LoadScript, InfoWindow } from '@react-google-maps/api';
 import ReactSlider from 'react-slider';
-import { css }from "emotion";
+import { css } from 'emotion';
 
 import { Link } from 'react-router-dom';
 import Embassy from '../../svgs/Embassy';
@@ -18,23 +18,25 @@ const StyledSlider = styled(ReactSlider)`
 `;
 
 const StyledThumb = styled.div`
-    height: 25px;
-    line-height: 25px;
-    width: 50px;
-    text-align: center;
-    background-color: #000;
-    color: #fff;
-    border-radius: 1rem;
-    cursor: grab;
+  height: 25px;
+  line-height: 25px;
+  width: 50px;
+  text-align: center;
+  background-color: #000;
+  color: #fff;
+  border-radius: 1rem;
+  cursor: grab;
 `;
 
-const Thumb = (props, state) => <StyledThumb {...props}>{state.valueNow}</StyledThumb>;
+const Thumb = (props, state) => (
+  <StyledThumb {...props}>{state.valueNow}</StyledThumb>
+);
 
 const StyledTrack = styled.div`
-    top: 0;
-    bottom: 0;
+  top: 0;
+  bottom: 0;
   background: #dddddd;
-    border-radius: 999px;
+  border-radius: 999px;
 `;
 
 const Track = (props, state) => <StyledTrack {...props} index={state.index} />;
@@ -45,96 +47,81 @@ const containerStyle = {
 
 const center = {
   lat: 35.9606,
-  lng:  -83.9207
+  lng: -83.9207,
 };
 
 const visibility = (currentYear, endyear, term) => {
   let vis = 1;
   const start = endyear - term;
   if (currentYear >= endyear) {
-   return 1;
+    return 1;
   }
 
-  if(endyear > currentYear && start <= currentYear){
-    vis = ((term - (endyear - currentYear)) / term);
+  if (endyear > currentYear && start <= currentYear) {
+    vis = (term - (endyear - currentYear)) / term;
   }
-  if (start > currentYear){
+  if (start > currentYear) {
     vis = 0;
   }
   return vis;
-}
+};
 
 export default () => {
   const [year, setYear] = useState(2020);
   const sites = useRdsData();
 
-  return (
-    sites
-    ? (
-      <div
-        className={css`
-        `}
-      >
-        <StyledSlider
-          min={2003}
-          max={2030}
-          defaultValue={[2020]}
-          renderTrack={Track}
-          renderThumb={Thumb}
-          onChange={value => setYear(value)}
-        />
-        <LoadScript
-          googleMapsApiKey="AIzaSyCsAfa4WqASPDbyECxtESeuJp66WC2kYzM"
-        >
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={14}
-          >
-            {
-              sites.map((site) => (
-                <InfoWindow
-                  key={site.project_name}
-                  position={{lat: site.lat, lng: site.lng}}
-                >
-                  <Link to={`/projects/${toKebabCase(site.project_name)}`}>
-                  <Embassy
-                    width="30px"
-                    style={{
-                      opacity: visibility(year, site.end_date, site.term)
-                    }}
-                    className={css`
-                      position: relative;
-                      z-index: 500;
-                      &:hover {
-                        + p {
-                          opacity: 1;
+  return sites ? (
+    <div className={css``}>
+      <StyledSlider
+        min={2003}
+        max={2030}
+        defaultValue={[2020]}
+        renderTrack={Track}
+        renderThumb={Thumb}
+        onChange={(value) => setYear(value)}
+      />
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={14}>
+          {sites.map((site) => (
+            <InfoWindow
+              key={site.project_name}
+              position={{ lat: site.lat, lng: site.lng }}>
+              <Link to={`/projects/${toKebabCase(site.project_name)}`}>
+                <Embassy
+                  width="30px"
+                  style={{
+                    opacity: visibility(year, site.end_date, site.term),
+                  }}
+                  className={css`
+                    position: relative;
+                    z-index: 500;
+                    &:hover {
+                      + p {
+                        opacity: 1;
                       }
-                      }
-                    `}
-                  />
-                    <p className={css`
-                      position: relative;
-                      z-index: 1000;
-                      opacity: 0;
+                    }
+                  `}
+                />
+                <p
+                  className={css`
+                    position: relative;
+                    z-index: 1000;
+                    opacity: 0;
                     margin-top: 0;
                     color: #000;
                     background: #fff;
                     border-radius: 1rem;
                     border: 1px #fff solid;
-                      padding: 0.5rem;
-                      transform: translateX(-50%);
-                    `}
-                  >
-                    {site.project_name}
-                  </p>
-                  </Link>
-                </InfoWindow>
-              ))
-            }
-          </GoogleMap>
-        </LoadScript>
-      </div>
-      ) : null
-  )
-}
+                    padding: 0.5rem;
+                    transform: translateX(-50%);
+                  `}>
+                  {site.project_name}
+                </p>
+              </Link>
+            </InfoWindow>
+          ))}
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  ) : null;
+};
